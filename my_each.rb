403e4e6a650a8086c613149a3784ebf7ca1d5ca7
luijocroussett <ljcroussett@gmail.com>
2018-11-11 17:04:10 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(array_to_iterate) # put argument(s) here
   # code here
+  counter = 0
+  while counter < array_to_iterate.size
+    yield array_to_iterate[counter]
+    counter += 1
+  end
+  array_to_iterate
 end
